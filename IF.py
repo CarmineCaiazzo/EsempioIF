4839dfb9330c1bcd3c8363f44948ef10dd971222
Kheruss <44398462+CarmineCaiazzo@users.noreply.github.com>
2018-11-26 17:12:36 +0100

    Add files via upload

---
+++ IF.py
@@ -0,0 +1,10 @@
+#Esercizio IF
+voto = input('inserisci voto: ')
+voto = int(voto)
+
+if voto<6:
+   print ("bocciato!!")
+elif voto<8:
+   print ("Potevi fare di meglio,NABBOH!!")
+else:
+   print ("promosso!!")
